ca7c04ccb8392ad40dee5dd999fe45134535499e
mhyoc <105059385+mhyoc@users.noreply.github.com>
2024-09-11 13:37:49 +0900

    update

--- Assets/Samples/OpenAI Unity/0.2.2/Whisper/Whisper.cs
+++ Assets/Samples/OpenAI Unity/0.2.2/Whisper/Whisper.cs
@@ -34,7 +34,8 @@ namespace Samples.Whisper
         private void Start()
         {
             UpdateMessage(message.text);
-            string apiKey = "sk-None-rKwUFueuFMNDdN1miXaST3BlbkFJzEmVKlJszn49bKlchik9";
+            // string apiKey = "sk-None-rKwUFueuFMNDdN1miXaST3BlbkFJzEmVKlJszn49bKlchik9";
+            string apiKey = "sk-ovOyYrGcrwsEPaZzgMLXYgnAQc3Yp2WKAWM6GgnrzDT3BlbkFJTKVN04_pqLKThwA60BOr4y9eiPdI496s4chKXwgiQA";
             openai = new OpenAIApi(apiKey);
 
             #if UNITY_WEBGL && !UNITY_EDITOR
