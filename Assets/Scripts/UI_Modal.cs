4c042bc923df23f019cbbe9f0aee3fc8de87b127
mhyoc <105059385+mhyoc@users.noreply.github.com>
2024-09-12 11:47:27 +0900

    modify

--- Assets/Scripts/UI_Modal.cs
+++ Assets/Scripts/UI_Modal.cs
@@ -1,53 +1,58 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
+
 public class UIModal : MonoBehaviour {
     public Image menuImage;
-    public GameObject cartModal;
     public TextMeshProUGUI title;
     public TextMeshProUGUI price;
     public GameObject OptionSlide;
+    public GameObject cartModal;
 
     private void Start() {
-        gameObject.SetActive(false);
+        transform.gameObject.SetActive(false);
     }
 
     public void OpenModal(string menuName, int menuPrice, string imageURL) {
         ResetModal();
+        
         // UICarousel optionCarousel = OptionSlide.GetComponent<UICarousel>();
-        // if (optionCarousel != null) {
+        // if (optionCarousel != null)
+        // {
         //     optionCarousel.SetPage(1);
         // }
-
-        gameObject.SetActive(true);
+        //
+        
+        transform.gameObject.SetActive(true);
 
         title.text = menuName;
         price.text = menuPrice.ToString();
         Sprite image = Resources.Load<Sprite>("Menu/" + imageURL);
         if (image != null) {
             menuImage.sprite = image;
-        } else {
-            Debug.LogWarning("Image not found: " + imageURL);
         }
     }
 
-    public void OpenCart() {
+    public void CartModal()
+    {
         cartModal.SetActive(true);
     }
 
     public void CloseModal() {
-        gameObject.SetActive(false);
+        transform.gameObject.SetActive(false);
         cartModal.SetActive(false);
     }
 
     public void ResetModal() {
-        ModalReset[] resetComponents = GetComponentsInChildren<ModalReset>(true);
-        foreach (ModalReset component in resetComponents) {
-            component.ModalOptionReset();
+        ModalReset[] resetComponents = transform.GetComponentsInChildren<ModalReset>(true);
+        foreach (ModalReset components in resetComponents) {
+            components.ModalOptionReset();
         }
+
     }
 }
