5980d268d32495a5b7d72e91cfac1c5735e7c04e
mhyoc <105059385+mhyoc@users.noreply.github.com>
2024-10-16 04:31:31 +0900

    매장 포장 카테고리 음성 추가

---
+++ Assets/Scripts/SelectSpeech.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Samples.Whisper;
+
+public class SelectSpeech : MonoBehaviour
+{
+    public AudioSource startAudio;
+    public GameObject STT;
+    private bool AudioPlayed = false;
+
+    public Whisper whisper;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(SceneManager.GetActiveScene().name == "SelectWhere")
+        {
+            StartScene();
+        }
+
+        if(SceneManager.GetActiveScene().name == "MenuPage")
+        {
+            StartScene();
+        }
+    }
+
+    void Update()
+    {
+        if(AudioPlayed && !startAudio.isPlaying)
+        {
+            STT.SetActive(true);
+            whisper.StartRecording();
+            AudioPlayed = false;
+        }
+    }
+
+    public void StartScene()
+    {
+        startAudio.Play();
+        AudioPlayed = true;
+    }
+}
